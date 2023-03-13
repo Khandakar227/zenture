245ff908a52961e91c08150e408d8dcec954b5f2
Khandakar Shakib <xenonknight54@gmail.com>
2023-03-14 01:39:55 +0600

    mode variable inside App

--- libs.py
+++ libs.py
@@ -4,15 +4,15 @@ from tkinter import Frame
 BUTTON_FONT = ('Arial', 12, 'bold')
 
 
-def button(frame: Frame, text, **kwargs):
+def button(frame: Frame, text, **kwargs)->tk.Button:
     return tk.Button(frame, text=text, width=25, font=BUTTON_FONT,
                      bg='#5dff5d', fg='black', borderwidth=5, anchor=tk.CENTER, **kwargs)
 
 
-def back_button(frame: Frame, **kwargs):
+def back_button(frame: Frame, **kwargs)->tk.Button:
     return tk.Button(frame, text="Back", font=BUTTON_FONT, anchor=tk.CENTER, **kwargs)
 
 
-def activate_button(frame: Frame, text: str, **kwargs):
+def activate_button(frame: Frame, text: str, **kwargs)->tk.Button:
     return tk.Button(frame, text=text, width=25, font=BUTTON_FONT,
-                     bg='#EA3A0D', fg='white', anchor=tk.CENTER, borderwidth=2, **kwargs)
+                     bg='#5dff5d', fg='black', anchor=tk.CENTER, borderwidth=2, **kwargs)
--- main.py
+++ main.py
@@ -1,22 +1,21 @@
 import tkinter as tk
-from tkinter import ttk
+# from tkinter import ttk
 from libs import button, back_button, activate_button
 
 LARGEFONT = ("Arial", 35)
 BG_COLOR = '#171A21'
 
+
 def toggle_mode(new_mode, frame: tk.Frame):
-    global mode
-    mode.get()
-    mode.set(new_mode if mode.get() != new_mode else 0)
-    
-    print(mode.get())
-    if mode.get() == new_mode:
+    global mod
+    app.mode.set(new_mode if app.mode.get() != new_mode else 0)
+    print(app.mode.get())
+    if app.mode.get() == new_mode:
         frame.toggle_activate.config(
-            text=f"Deactivate", bg='#5dff5d', fg='black')
+            text=f"Deactivate", bg='#EA3A0D', fg='white')
     else:
         frame.toggle_activate.config(
-            text=f"Activate", bg='#EA3A0D', fg='white')
+            text=f"Activate", bg='#5dff5d', fg='black')
 
 
 class App(tk.Tk):
@@ -24,6 +23,7 @@ class App(tk.Tk):
     def __init__(self, *args, **kwargs):
         # __init__ function for class Tk
         tk.Tk.__init__(self, *args, **kwargs)
+        self.mode = tk.IntVar()
         self.configure(bg=BG_COLOR)
         self.geometry('600x600')
         self.title('Zenture')
@@ -32,6 +32,10 @@ class App(tk.Tk):
         container.pack(fill="none", expand=True)
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
+        # An auto update variable for gesture mode
+        # Which gesture control to use
+        self.mode.initialize(0)
+        self.mode.trace('w', self.on_change_mode)
 
         # initializing frames to an empty array
         self.frames = {}
@@ -44,11 +48,52 @@ class App(tk.Tk):
             frame.grid(row=0, column=0, sticky="nsew")
         self.show_frame(MainPage)
 
+        # Status bar
+        self.statusbar = tk.Label(self, text="No mode is active", fg="red", bg='black', font=('Verdana', 11), relief=tk.SUNKEN, anchor="w")
+        self.statusbar.pack(side=tk.BOTTOM, fill=tk.X)
+
     # to display the current frame passed as
     # parameter
     def show_frame(self, cont):
         frame = self.frames[cont]
         frame.tkraise()
+
+    def on_change_mode(self, *args):
+        if self.mode.get() == 1:
+            self.statusbar.configure(text='Dual hand gesture is active', fg='lightgreen')
+            print(self.mode.get())
+            
+            # Dual hand gesture code
+            
+            pass
+        
+        elif self.mode.get() == 2:
+            self.statusbar.configure(text='Full body gesture is active', fg='lightgreen')
+            print(self.mode.get())
+        
+            # Full body gesture code
+        
+            pass
+        
+        elif self.mode.get() == 3:
+            self.statusbar.configure(text='Aircraft steering gesture is active', fg='lightgreen')
+            print(self.mode.get())
+            # Aircraft steering code
+            pass
+        elif self.mode.get() == 4:
+            self.statusbar.configure(text='Single hand gesture is active', fg='lightgreen')
+            print(self.mode.get())
+            # Single Hand code
+            pass
+        elif self.mode.get() == 5:
+            self.statusbar.configure(text='Racing gesture is active', fg='lightgreen')
+            print(self.mode.get())
+            # Racing gesture code
+            pass
+        else:
+            self.statusbar.configure(text='No mode is active', fg='red')
+            print(self.mode.get())
+            # No gesture code / deactivate
 
 
 class MainPage(tk.Frame):
@@ -66,11 +111,11 @@ class MainPage(tk.Frame):
         racing_btn = button(self, text='Racing',
                             command=lambda: controller.show_frame(RacingPage))
 
-        dual_hand_btn.pack(pady=15, padx=15)
-        full_body_btn.pack(pady=15, padx=15)
-        steering.pack(pady=15, padx=15)
-        single_hand_btn.pack(pady=15, padx=15)
-        racing_btn.pack(pady=15, padx=15)
+        dual_hand_btn.grid(column=0, row=1, pady=15, padx=15)
+        full_body_btn.grid(column=0, row=2, pady=15, padx=15)
+        steering.grid(column=0, row=3, pady=15, padx=15)
+        single_hand_btn.grid(column=0, row=4, pady=15, padx=15)
+        racing_btn.grid(column=0, row=5, pady=15, padx=15)
 
 
 class SubPage(tk.Frame):
@@ -166,12 +211,6 @@ def on_change_mode(*args):
         print(mode.get())
         # No gesture code / deactivate
 
+
 app = App()
-
-# An auto update variable for gesture mode
-# Which gesture control to use
-mode = tk.IntVar(app)
-mode.initialize(0)
-
-mode.trace('w', on_change_mode)
 app.mainloop()
